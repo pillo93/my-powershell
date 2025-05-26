0c5e9f7628b103227da3f78d33f30ed89fd6514a
lpilastri93 <pilastri_leonardo@yahoo.it>
2025-05-26 15:35:24 +0200

    Fix curl repox

--- curl/repox.ps1
+++ curl/repox.ps1
@@ -5,7 +5,10 @@ function repox.list(){
         [Parameter(Mandatory=$true, Position=1)]
         [string]$ArtifactName
     )
-    Invoke-WebRequest -Uri "https://repox.jfrog.io/repox/$RepositoryName/org/sonarsource/java/$ArtifactName/maven-metadata.xml" `
-    -Headers @{ "Authorization" = "Bearer $($ARTIFACTORY_PASSWORD)" } `
-    -OutFile $null # -OutFile $null is equivalent to -o - (output to stdout) in curl for this specific use case, though you might want to redirect to a file if you need to save it.
+    $url = "https://repox.jfrog.io/repox/${RepositoryName}/org/sonarsource/java/${ArtifactName}/maven-metadata.xml"
+    Write-Output $url
+    $Response = Invoke-WebRequest -Uri  $url `
+    -Headers @{ "Authorization" = "Bearer $($env:ARTIFACTORY_PASSWORD)" } `
+
+    $Response.Content
 }
